5b1c9d44cfa79fb5140289c153c58c7c986510a8
Aaron Leventhal <aleventhal@google.com>
2019-04-11 11:23:57 -0400

    Message from content now reaches extension

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onConnect.addListener((port) => {
+chrome.runtime.onConnectExternal.addListener((port) => {
 
   if (chrome.runtime.lastError) {
     console.error(chrome.runtime.lastError);
@@ -12,69 +12,29 @@ chrome.runtime.onConnect.addListener((port) => {
   // object.
   const requestType = params[1];
   const uid = params[2];
-  console.log('Im')
+  console.log('Connect', params);
 
 
+  if (true)
+    return;
 
-  port.onDisconnect.addListener(() => onPortDisconnect(thing));
+  //port.onDisconnect.addListener(() => onPortDisconnect(thing));
   port.onMessage.addListener(onPortMessage);
 
-  console.info('Connected to automation client with tab id = ' + tabId);
-
-  const automationTree = new AutomationTree(port, tabId);
-  automationTree.init()
-  .then((response) => {
-    if (portToTreeMap.size === 0) {
-      onFirstConnectionAdded();
-    }
-    const rootNode = response;
-    portToTreeMap.set(tabId, automationTree);
-    rootToTreeMap.set(rootNode, automationTree);
-    const rootNodeData = automationTree.getNodeData(rootNode);
-    const allStates = Object.values(chrome.automation.StateType);
-    const allRoles = Object.values(chrome.automation.RoleType);
-
-    port.postMessage({
-      message: 'ready',
-      tab: tabId,
-      rootNode: rootNodeData,
-      allStates,
-      allRoles
-    });
-  })
-  .catch((error) => {
-    port.postMessage({
-      message: 'error',
-      tab: tabId,
-      error: error.toString()
-    });
-  });
+    // port.postMessage({
+    //   message: 'ready',
+    //   tab: tabId,
+    //   rootNode: rootNodeData,
+    //   allStates,
+    //   allRoles
+    // });
 });
-
-function onFirstConnectionAdded() {
-  console.info('Begin listening to treeChanges');
-  chrome.automation.addTreeChangeObserver('allTreeChanges', onTreeChange);
-}
-
-function onLastConnectionRemoved() {
-  console.info('End listening to treeChanges');
-  chrome.automation.removeTreeChangeObserver(onTreeChange);
-}
 
 // We do not currently receive port messages, only send them
 function onPortMessage() {
-  throw new Error('Port message unexpected');
+  //throw new Error('Port message unexpected');
 }
 
-function onPortDisconnect(tabId) {
-  const tree = getTree(tabId);
-  if (tree) {
-    tree.removeEventListeners();
-    rootToTreeMap.delete(tree.rootNode);
-    portToTreeMap.delete(tabId);
-    if (portToTreeMap.size === 0) {
-      onLastConnectionRemoved();
-    }
-  }
+function onPortDisconnect() {
 }
 
