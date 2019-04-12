316bfd48482e36a91928355173f5da14bdc56840
Aaron Leventhal <aleventhal@google.com>
2019-04-12 09:28:32 -0400

    Add port disconnect listener

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -12,12 +12,14 @@ chrome.runtime.onConnectExternal.addListener((port) => {
 
   if (!port.name.startsWith('js2at::'))
     return;  // Not handled.
+
+  port.onDisconnect.addListener(onPortDisconnect);
   const params = port.name.split('::');
   // A new port has opened to listen to a single request type on a single
   // object.
   const requestType = params[1];
   targetUid = params[2];
-  console.log('Connected!', params);
+  console.log('Connected!', port);
   contentRequestPort = port;
 
   port.onMessage.addListener(onPortMessage);
@@ -47,6 +49,7 @@ function onPortMessage(message) {
   console.log('Message received!', message);
 }
 
-function onPortDisconnect() {
+function onPortDisconnect(port) {
+  console.log('Port disconnected', port.name);
 }
 
