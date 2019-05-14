05d237f6370e21f6dde3853d6e4bd43480d6bf39
Aaron Leventhal <aleventhal@google.com>
2019-05-14 13:52:26 -0400

    Add copyright notices

--- message-broker/install-broker-win.bat
+++ message-broker/install-broker-win.bat
@@ -1,6 +1,20 @@
-:: Change HKLM to HKLM if you want to install globally.
+rem  Copyright 2019 Google LLC
+rem
+rem  Licensed under the Apache License, Version 2.0 (the "License");
+rem  you may not use this file except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem
+rem       http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem  Unless required by applicable law or agreed to in writing, software
+rem  distributed under the License is distributed on an "AS IS" BASIS,
+rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem  See the License for the specific language governing permissions and
+rem  limitations under the License.
+
+:: Change HKLM to HKCU if you want to install only for current user.
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
-:: Install for all channels of Chrome or Chromium.
+:: Install for all channels of Chrome, Chromium and Firefox.
 REG ADD "HKLM\Software\Google\Chrome\NativeMessagingHosts\org.js2at.message_broker" /ve /t REG_SZ /d "%~dp0org.js2at.message_broker-win.json" /f
 REG ADD "HKLM\Software\Google\Chrome Beta\NativeMessagingHosts\org.js2at.message_broker" /ve /t REG_SZ /d "%~dp0org.js2at.message_broker-win.json" /f
 REG ADD "HKLM\Software\Google\Chrome Dev\NativeMessagingHosts\org.js2at.message_broker" /ve /t REG_SZ /d "%~dp0org.js2at.message_broker-win.json" /f
--- message-broker/uninstall-broker-win.bat
+++ message-broker/uninstall-broker-win.bat
@@ -1,3 +1,17 @@
+rem  Copyright 2019 Google LLC
+rem
+rem  Licensed under the Apache License, Version 2.0 (the "License");
+rem  you may not use this file except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem
+rem       http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem  Unless required by applicable law or agreed to in writing, software
+rem  distributed under the License is distributed on an "AS IS" BASIS,
+rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem  See the License for the specific language governing permissions and
+rem  limitations under the License.
+
 :: Deletes the entries created by install_broker.bat
 REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\org.js2at.message_host" /f
 REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\org.js2at.message_host" /f
